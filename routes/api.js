2c45d72950d733fcdb0587b89ba21c4afcf70f57
JonelleH <jonelle.harris04@gmail.com>
2022-02-03 19:46:29 -0500

    added mongodb

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://jonelleh:Double007@cluster0.eiyxu.mongodb.net/Mongodb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
